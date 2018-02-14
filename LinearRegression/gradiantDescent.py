8b3f05823092fe23af856b7b5ca5a1981713c23b
Daggerron <cmalfroy@student.42.fr>
2018-02-14 19:22:02 +0100

    better error handling

--- LinearRegression/gradiantDescent.py
+++ LinearRegression/gradiantDescent.py
@@ -24,21 +24,24 @@ if __name__ == '__main__':
 
     tabdata = readdata()
     m = len(tabdata)
-    learningrate = 1e-10
-    teta0 = 0
-    teta1 = 0
-    for i in range(0, 5100):
-        sum1 = 0
-        sum2 = 0
+    learningrate = 0.3
+    teta0 = 0.0
+    teta1 = 0.0
+    for i in range(0, 300):
+        sum1 = 0.0
+        sum2 = 0.0
         print (teta0)
         for data in tabdata:
             cost = (teta0 + teta1 * data[0]) - data[1]
             sum1 = sum1 + cost
             sum2 = sum2 + cost * data[0]
-        minimalize = learningrate * 1 / m
+        minimalize = learningrate * 1.0 / float(m)
         teta0 -= minimalize * sum1
         teta1 -= minimalize * sum2
-    nb = int(input("Entrez un nombre a estimer: "))
+    nb = input("Entrez un nombre a estimer: ")
+    if (not nb.isnumeric()):
+        print('Mauvais format!')
+        exit(0)
     print(teta0)
     print(teta1)
-    print(estimate(teta0, teta1, nb))
+    print(estimate(teta0, teta1, int(nb)))
