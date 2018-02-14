132217e6bbb4fd04c6707a0bdd7e80b226a4a8e3
Daggerron <cmalfroy@student.42.fr>
2018-02-14 18:40:48 +0100

    parsing right but wrong output value

--- LinearRegression/gradiantDescent.py
+++ LinearRegression/gradiantDescent.py
@@ -1,21 +1,44 @@
+import csv
+
 def estimate(t0, t1, a):
     return (t0 + (t1 * a))
 
+def readdata():
+    tabdata = []
+    with open('data.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            new = []
+            c = True
+            row = row[0].split(",")
+            for elem in row:
+                if not elem.isdigit():
+                    c = False
+                else:
+                    new.append(int(elem))
+            if c == True:
+                tabdata.append(new)
+    return (tabdata)
+
 if __name__ == '__main__':
-    tabdata = [[0, 5], [0, 6], [1, 4], [3, 2], [4, 1]]
+
+    tabdata = readdata()
     m = len(tabdata)
-    alpha = 0.2
+    learningrate = 1e-10
     teta0 = 0
     teta1 = 0
-    for i in range(0, m * 100):
+    for i in range(0, 5100):
         sum1 = 0
         sum2 = 0
+        print (teta0)
         for data in tabdata:
             cost = (teta0 + teta1 * data[0]) - data[1]
             sum1 = sum1 + cost
             sum2 = sum2 + cost * data[0]
-        minimalize = alpha * 1 / m
+        minimalize = learningrate * 1 / m
         teta0 -= minimalize * sum1
         teta1 -= minimalize * sum2
     nb = int(input("Entrez un nombre a estimer: "))
+    print(teta0)
+    print(teta1)
     print(estimate(teta0, teta1, nb))
