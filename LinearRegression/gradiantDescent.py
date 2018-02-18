d73f4fc01ce1f95f2cd8fbb8922c51075fb68bae
Daggerron <cmalfroy@student.42.fr>
2018-02-18 13:31:29 +0100

    might be better but still not good

--- LinearRegression/gradiantDescent.py
+++ LinearRegression/gradiantDescent.py
@@ -25,21 +25,20 @@ if __name__ == '__main__':
     dataset = getdataset()
     m = len(dataset)
     learningrate = 1e-6
-    teta0 = 0.0
-    teta1 = 0.0
+    theta0 = 0.0
+    theta1 = 0.0
     minimalize = learningrate * (1.0 / m)
     for i in range(0, 30):
         sumT0 = 0.0
         sumT1 = 0.0
-        print("teta0: ", teta0, "       teta1: ", teta1)
         for data in dataset:
-            cost = estimate(teta0, teta1, data[0]) - data[1]
+            cost = estimate(theta0, theta1, data[0]) - data[1]
             sumT0 += cost
             sumT1 += cost * data[0]
-        teta0 = minimalize * (sumT0 / m)
-        teta1 = minimalize * (sumT1 / m)
+        theta0 = minimalize * (sumT0 / m)
+        theta1 = minimalize * (sumT1 / m)
     nb = input("Enter a mileage: ")
     if (not nb.isnumeric()):
         print('Wrong format!')
         exit(0)
-    print(estimate(teta0, teta1, int(nb)))
+    print(estimate(theta0, theta1, int(nb)))
