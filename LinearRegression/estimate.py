f7967cc4d78093f5f6207f9832c3d1d4434d1473
cmalfroy <cmalfroy@student.42.fr>
2018-02-22 11:20:32 +0100

    import from file avoid double function definition

--- LinearRegression/estimate.py
+++ LinearRegression/estimate.py
@@ -1,8 +1,5 @@
 import matplotlib.pyplot as plt
-import csv
-
-def estimate(t0, t1, a):
-    return (t0 + (t1 * a))
+import gradiantDescent as gd
 
 def gettheta():
     try:
@@ -14,23 +11,6 @@ def gettheta():
         theta = [0, 0]
     return (theta)
 
-def getdataset():
-    tabdata = []
-    with open('data.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            new = []
-            c = True
-            row = row[0].split(",")
-            for elem in row:
-                if not elem.isdigit():
-                    c = False
-                else:
-                    new.append(int(elem))
-            if c == True:
-                tabdata.append(new)
-    return (tabdata)
-
 def display(theta, dataset):
     nb = input("Enter a mileage: ")
     if (not nb.isnumeric()):
@@ -38,15 +18,15 @@ def display(theta, dataset):
         exit(0)
     for data in dataset:
         plt.plot(data[0], data[1], "bo")
-    plt.plot([250000, 100], [estimate(theta[0], theta[1], 250000), estimate(theta[0], theta[1], 100)], 'r-')
-    print("With:", nb, "km mileage, you can expect a price of: ", estimate(theta[0], theta[1], int(nb)), "dollars.")
-    plt.plot(int(nb), estimate(theta[0], theta[1], int(nb)), "go")
+    plt.plot([250000, 100], [gd.estimate(theta[0], theta[1], 250000), gd.estimate(theta[0], theta[1], 100)], 'r-')
+    print("With:", nb, "km mileage, you can expect a price of: ", gd.estimate(theta[0], theta[1], int(nb)), "dollars.")
+    plt.plot(int(nb), gd.estimate(theta[0], theta[1], int(nb)), "go")
     plt.show()
 
 if __name__ == '__main__':
     theta = gettheta()
     if (theta[0] != 0 or theta[1] != 0):
-        dataset = getdataset()
+        dataset = gd.getdataset()
         display(theta, dataset)
     else:
         print("You have not trained you model yet.")
