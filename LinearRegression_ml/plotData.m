c6fbe8fd45b7cf88f43e4c7c9da1fcbb9c68f4be
cmalfroy <cmalfroy@student.42.fr>
2018-02-22 18:23:06 +0100

    Linear regression in Octave

--- LinearRegression_ml/plotData.m
+++ LinearRegression_ml/plotData.m
@@ -4,24 +4,8 @@ function plotData(x, y)
 %   population and profit.
 
 figure; % open a new figure window
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
-%
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
-
-figure(1), plot(x, y, 'rx', 'MarkerSize', 10);
+plot(x, y, 'rx', 'MarkerSize', 10);
 xlabel('population');
 ylabel('revenue');
-
-
-% ============================================================
 
 end
